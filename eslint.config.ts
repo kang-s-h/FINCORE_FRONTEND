aaae1ae6e4ae815330e6b0828e5c37cbc5e078d7
강성훈 <kshevl123@gmail.com>
2026-03-20 16:33:34 +0900

    chore: prettier 설치

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,7 +2,6 @@ import js from '@eslint/js';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import prettierConfig from 'eslint-config-prettier';
 import importPlugin from 'eslint-plugin-import';
-import jsxA11y from 'eslint-plugin-jsx-a11y';
 import prettierPlugin from 'eslint-plugin-prettier';
 import reactPlugin from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
@@ -31,7 +30,6 @@ export default defineConfig([
     },
     plugins: {
       react: reactPlugin,
-      'jsx-a11y': jsxA11y,
       import: importPlugin,
       prettier: prettierPlugin,
     },
